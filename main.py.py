8d8ac45e2b0a542db2ec2bd5200c40030c1c0bd5
Raymond9266 <92591008+Raymond9266@users.noreply.github.com>
2022-04-08 01:29:49 +0800

    Add files via upload

---
+++ main.py.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+option = webdriver.ChromeOptions()
+option.add_argument("-incognito")
+
+browser = webdriver.Chrome(executable_path='YOURPATH' , options=option)
+
+browser.get("http://1.1.1.2/")
+
+# Use the following snippets to get elements by their XPath
+id = browser.find_elements_by_xpath("/html/body/div[1]/div/div/div[1]/div/form/div[1]/input")
+password = browser.find_elements_by_xpath("/html/body/div[1]/div/div/div[1]/div/form/div[2]/input")
+browser.execute_script("arguments[0].click();", browser.find_element(By.ID,"tnc"))
+submit = browser.find_elements_by_xpath("/html/body/div[1]/div/div/div[1]/div/form/input")
+
+id[0].send_keys("YOURUTMID")
+password[0].send_keys("YOURPASSWORD")
+submit[0].click()
+
+time.sleep(2)
+browser.close()
