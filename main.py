92399b4183ffafd884c66c88cf5fef88098d8ffe
rcmf-9266 <92591008+rcmf-9266@users.noreply.github.com>
2022-04-12 11:40:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 import time
 
 option = webdriver.ChromeOptions()
 option.add_argument("-incognito")
-
-browser = webdriver.Chrome(executable_path='YOURPATH' , options=option)
+ser = Service("YOUR_PATH") #Example: C:\\Users\ACER\Downloads\chromedriver.exe
+browser = webdriver.Chrome(service=ser , options=option)
 
 browser.get("http://1.1.1.2/")
 
-id = browser.find_elements_by_xpath("/html/body/div[1]/div/div/div[1]/div/form/div[1]/input")
-password = browser.find_elements_by_xpath("/html/body/div[1]/div/div/div[1]/div/form/div[2]/input")
-browser.execute_script("arguments[0].click();", browser.find_element(By.ID,"tnc"))
-submit = browser.find_elements_by_xpath("/html/body/div[1]/div/div/div[1]/div/form/input")
+# Use the following snippets to get elements by their XPath
+try:
+    name = browser.find_elements(by=By.XPATH, value="/html/body/div[1]/div/div/div[1]/div/form/div[1]/input")
+    password = browser.find_elements(by=By.XPATH,value="/html/body/div[1]/div/div/div[1]/div/form/div[2]/input")
+    browser.execute_script("arguments[0].click();", browser.find_element(By.ID,"tnc"))
+    submit = browser.find_elements(by=By.XPATH, value="/html/body/div[1]/div/div/div[1]/div/form/input")
 
-id[0].send_keys("YOURUTMID")
-password[0].send_keys("YOURPASSWORD")
-submit[0].click()
+    name[0].send_keys("YOUR_UTMID")
+    password[0].send_keys("YOUR_PASSWORD")
+    submit[0].click()
 
-time.sleep(2)
-browser.close()
+    time.sleep(2)
+    browser.close()
+except:
+    print("Error! Please make sure you are not logged in!")
